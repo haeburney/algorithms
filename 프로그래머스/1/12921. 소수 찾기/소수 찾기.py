51557b11d92cc1b377d0a419bbcd03b4097fcfb0
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2026-04-28 14:22:30 +0900

    [level 1] Title: 소수 찾기, Time: 1230.45 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,20 +1,17 @@
 def solution(n):
-    decimal_number = []
-    
-    if(n < 2) : 
-        return answer
+    prime_number = []
 
     for i in range(2, n + 1):
         j = 1
         count = 0
-        for num in decimal_number:
-            if(num*num) > i :
+        for num in prime_number:
+            if(num*num) > i : # 제곱근까지만 검사하기 
                 break
-            if(i % num == 0):
-                count += 1
+            if(i % num == 0) : # 소수가 아니라면 count++해주고 바로 break로 빠져나오기  
+                count += 1 
                 break
                 
         if(count == 0):
-            decimal_number.append(i)
+            prime_number.append(i)
     
-    return len(decimal_number)
+    return len(prime_number)
