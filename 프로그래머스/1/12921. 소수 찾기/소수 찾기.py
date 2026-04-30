111d75beb056f98e54ba0d3d07080dfbab4573f6
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2026-04-30 14:45:49 +0900

    [level 1] Title: 소수 찾기, Time: 1127.27 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,17 +1,13 @@
 def solution(n):
     prime_number = []
-
     for i in range(2, n + 1):
-        j = 1
-        count = 0
         for num in prime_number:
-            if(num*num) > i : # 제곱근까지만 검사하기 
+            if num * num > i:
+                prime_number.append(i)
                 break
-            if(i % num == 0) : # 소수가 아니라면 count++해주고 바로 break로 빠져나오기  
-                count += 1 
+            if i % num == 0:
                 break
-                
-        if(count == 0):
+        else:
             prime_number.append(i)
     
     return len(prime_number)
