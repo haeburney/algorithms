54dfc96ba806a7f40f1954181ec0dbb0de6dad92
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2026-05-04 14:29:40 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    d.sort()
+    answer = 0
+    
+    for value in d:
+        if budget - value >= 0:
+            budget -= value
+            answer += 1
+
+    return answer
