8292d0e9d6e5c1b9132ae9b53b8382779db0402d
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2026-07-25 22:10:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.08 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,46 +1,28 @@
 def solution(survey, choices):
     answer = ''
-    count = [0, 0, 0, 0, 0, 0, 0, 0]
-    types = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
-    # R=0, T=1, C=2, F=3, J=4, M=5, A=6, N=7
     
-    for i in range(len(survey)):
-        input_type = list(survey[i])
+    type_scores = {
+        "R": 0, "T": 0,
+        "C": 0, "F": 0,
+        "J": 0, "M": 0,
+        "A": 0, "N": 0
+    }
+    
+    for category, choice in zip(survey, choices):
+        disagree_type = category[0]
+        agree_type = category[1]
         
-        # 알파벳 순서 정렬 및 점수 역전
-        if input_type[0] > input_type[1]:
-            input_type.sort()
-            choices[i] = 8 - choices[i]
+        if(choice >= 5):
+            type_scores[agree_type] += choice - 4
+        else :
+            type_scores[disagree_type] += 4 - choice
             
-        first_char = input_type[0]
-        choice = choices[i]
-        
-        if first_char == 'R':
-            if choice >= 4:
-                count[1] += choice - 4
-            else:
-                count[0] += 4 - choice
-        elif first_char == 'C':
-            if choice >= 4:
-                count[3] += choice - 4
-            else:
-                count[2] += 4 - choice
-        elif first_char == 'J':
-            if choice >= 4:
-                count[5] += choice - 4
-            else:
-                count[4] += 4 - choice
-        elif first_char == 'A':
-            if choice >= 4:
-                count[7] += choice - 4
-            else:
-                count[6] += 4 - choice
-
-    # 점수 비교 후 결과 문자열 생성
-    for i in range(4):
-        if count[i * 2] >= count[i * 2 + 1]:
-            answer += types[i * 2]
+    indicators = [("R", "T"), ("C","F"), ("J","M"), ("A","N")]
+    
+    for type_a, type_b in indicators:
+        if(type_scores[type_a] >= type_scores[type_b]):
+            answer += type_a
         else:
-            answer += types[i * 2 + 1]
+            answer += type_b
             
     return answer
